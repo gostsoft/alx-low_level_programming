620ab0e09b4da0ca1561bbfaef42e5dccacfe2e3
Gostsoft <mrjohn.soft@gmail.com>
2022-04-07 14:16:52 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,9 @@
 /** main - Entry point
  * Return - when 0 (success)
  */
+
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art\n");
+printf ("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
