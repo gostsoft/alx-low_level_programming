27f5a6634598bee97e8d09fb194b8552a3428045
Gostsoft <mrjohn.soft@gmail.com>
2022-04-07 14:21:18 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /** 
  * main - Entry point
- * Return - when 0 (success)
+ * Return: when 0 (success)
  */
 
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
