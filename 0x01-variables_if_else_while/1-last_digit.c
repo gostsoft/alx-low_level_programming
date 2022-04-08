47b9cdae8c0e448653992dc4b4be2c766198c7a9
Gostsoft <mrjohn.soft@gmail.com>
2022-04-08 14:49:37 -0700

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 l = n % 10;
 if (l > 5)
 {
-printf("Last digit %d is %d and is greater than 5\n", n, l);
+printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
 else if (l < 6 && l != 0)
 {
