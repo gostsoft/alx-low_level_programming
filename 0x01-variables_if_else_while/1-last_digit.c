dcbf64f26c82730899d4e49ff11980ccc939115a
Gostsoft <mrjohn.soft@gmail.com>
2022-04-08 14:46:11 -0700

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+
+
+
+/**
+*main - prints is posetive, negative
+* or zero
+	 *Return: returns zero if program exits well.
+	  */
+
 int main(void)
 {
 		int n;
