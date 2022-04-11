f9ea5320fb95f141090a49e432a504c77ced7ded
Gostsoft <mrjohn.soft@gmail.com>
2022-04-11 06:19:21 -0700

    m

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * * main - prints is posetive, negative
- * * or zero
- * *Return: returns zero if program exits well.
- *  */
+ * main - prints is posetive, negative
+ * or zero
+ *Return: returns zero if program exits well.
+ */
 int main(void)
 {
 	int c = 0;
+
 	while (c <= 9)
 	{
 		putchar(48 + c);
