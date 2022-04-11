c9e530fb47229b9294e42210df7c540f0fb2f70e
Gostsoft <mrjohn.soft@gmail.com>
2022-04-11 06:07:50 -0700

    m

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * * main - prints is posetive, negative
- * * or zero
- * *Return: returns zero if program exits well.
- *  */
+ * main - prints is posetive, negative
+ * or zero
+ *Return: returns zero if program exits well.
+ */
 int main(void)
 {
 	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
