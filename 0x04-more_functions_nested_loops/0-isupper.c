69c8c8a720bf686911ef84326a69ca5ab19ca17d
gostsoft <mrjohn.soft@gmail.com>
2022-04-14 13:04:23 +0000

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * * _isupper - evalue if letter is in uppercase.
+ * @c: print int
+ * Return: returns zero
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+}
