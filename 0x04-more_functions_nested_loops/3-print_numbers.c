4a9973ec4a49784eac579876cfa3236aaa514c1a
gostsoft <mrjohn.soft@gmail.com>
2022-04-14 13:42:54 +0000

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_numbers - print numbers
  * Return: Always 0.
@@ -10,7 +10,7 @@ void print_numbers(void)
 int c;
 for (c = 0; c <= 9; i++)
 {
-_putchar(c + '0');
-_putchar('\n');
+putchar(c + '0');
+putchar('\n');
 }
 }
