9181a90abc2ad8033be648cc98ae0c7a8ad558df
gostsoft <mrjohn.soft@gmail.com>
2022-04-14 13:38:09 +0000

    m

---
+++ 0x04-more_functions_nested_loops/putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * putchar -Prints out character
+ * @c: print char
+ * Return: returns zero
+ */
+int _putchar(char c)
+{
+putchar(c);
+return (0);
+}
