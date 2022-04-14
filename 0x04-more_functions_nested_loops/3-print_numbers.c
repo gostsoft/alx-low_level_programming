dfb79901cc39f29fd760edf39f1ac6fa84a3fc30
Gostsoft <mrjohn.soft@gmail.com>
2022-04-14 07:15:30 -0700

    good

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,16 @@
  * print_numbers - print numbers
  * Return: Always 0.
  */
-
 void print_numbers(void)
 {
-int c;
-for (c = 0; c < 10; i++)
-{
-_putchar(c + '0');
-_putchar('\n');
-}
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar (i + '0');
+		i++;
+	}
+
+	_putchar ('\n');
 }
