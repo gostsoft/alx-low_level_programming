b5390c020eebffa51628da12cbe7373b55b05b10
gostsoft <mrjohn.soft@gmail.com>
2022-04-14 16:35:05 +0000

    k

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,42 @@
-#include "main.h"
-
+nclude "main.h"
 /**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  *
- */
+ * Return: Always 0.
+ **/
 
 void more_numbers(void)
 {
-int i = 0;
-int j =0;
-while(j <= 10)
-{
-while (i <= 14)
-{
-_putchar(c);
-i++;
-}
-_putchar('\n');
-j++;
-}
+	int i;
+	int num1;
+	int num2;
+	int sum;
+
+	i = 0;
+	sum = 0;
+	while (i < 10)
+	{
+		while (sum <= 14)
+		{
+			if (sum < 10)
+			{
+				num2 = sum;
+			}
+
+			else
+			{
+				num1 = sum / 10;
+				num2 = sum % 10;
+				_putchar (num1 + '0');
+			}
+
+			_putchar (num2 + '0');
+
+			sum++;
+		}
+		i++;
+		sum = 0;
+		_putchar ('\n');
+	}
+
 }
