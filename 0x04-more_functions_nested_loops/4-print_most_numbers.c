358abefee5adc71e6652e3673dd1794b0adf8ca8
gostsoft <mrjohn.soft@gmail.com>
2022-04-14 14:32:09 +0000

    m

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,23 @@
 #include "main.h"
-
 /**
+ * print_most_numbers - print numbers
  *
+ * Return: Always 0.
  */
 void print_most_numbers(void)
 {
-int c = 0;
-while(c <= 9)
-{
-if (c == 2 && c == 4)
-{
-continue;
-}
-else
-{
-_putchar(c + '0');
-c++;
-}
 
-_putchar('\n');
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar (i + '0');
+		}
+		i++;
+	}
+
+	_putchar ('\n');
 }
